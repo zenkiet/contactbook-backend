4c8dcb275ca0f463148095e68fb15ed70648fa74
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-02-22 16:14:08 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const ContactRoutes = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -13,5 +14,18 @@ app.get("/", (req, res) => {
 });
 
 ContactRoutes(app);
+
+// handle 404 error
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});    
+
+// handle errors
+app.use((err, req, res, next) => {
+    if (err instanceof ApiError) {
+        return res.status(err.statusCode).json({ message: err.message });
+    }
+    return res.status(500).json({ message: err.message });
+})
 
 module.exports = app;
